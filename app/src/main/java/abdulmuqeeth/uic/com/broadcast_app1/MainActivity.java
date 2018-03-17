1a005c9b67ce32d341328573c2bf0302ea82efe7
abdulmuqeethmohammed <amoham62@uic.edu>
2018-03-17 18:02:03 -0500

    Broadcasting intents

--- app/src/main/java/abdulmuqeeth/uic/com/broadcast_app1/MainActivity.java
+++ app/src/main/java/abdulmuqeeth/uic/com/broadcast_app1/MainActivity.java
@@ -1,9 +1,11 @@
 package abdulmuqeeth.uic.com.broadcast_app1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener attractionListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-        //TODO Implement button1 functionality
+            Toast.makeText(getApplicationContext(), "Attractions" , Toast.LENGTH_SHORT).show();
+
+            Intent attraction = new Intent("abdulmuqeeth.uic.com.receiver_app.ATTRACTIONS");
+            sendBroadcast(attraction);
         }
     };
 
     private View.OnClickListener restaurantsListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-        //TODO implementing button2 functionality
+            Toast.makeText(getApplicationContext(), "Restaurants", Toast.LENGTH_SHORT).show();
+
+            Intent restaurant = new Intent("abdulmuqeeth.uic.com.receiver_app.RESTAURANTS");
+            sendBroadcast(restaurant);
         }
     };
 
