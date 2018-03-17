68856ae6d0dd5972a60dd17ffb73f65afec20299
abdulmuqeethmohammed <amoham62@uic.edu>
2018-03-17 17:33:13 -0500

    Defining button listeners

--- app/src/main/java/abdulmuqeeth/uic/com/broadcast_app1/MainActivity.java
+++ app/src/main/java/abdulmuqeeth/uic/com/broadcast_app1/MainActivity.java
@@ -2,6 +2,8 @@ package abdulmuqeeth.uic.com.broadcast_app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button attractions_button = (Button) findViewById(R.id.button1);
+        Button restaurants_button = (Button) findViewById(R.id.button2);
+
+        attractions_button.setOnClickListener(attractionListener);
+        restaurants_button.setOnClickListener(restaurantsListener);
     }
+
+    private View.OnClickListener attractionListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        //TODO Implement button1 functionality
+        }
+    };
+
+    private View.OnClickListener restaurantsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        //TODO implementing button2 functionality
+        }
+    };
+
+
 }
